39ae0453eaf076a14e0384ad5f920f8fedbd26db
Marcus Rohrmoser <github@mro.name>
2015-09-04 01:11:34 +0200

    test title

---
+++ scripts/test-title.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#
+#  Copyright (c) 2015 Marcus Rohrmoser http://mro.name/me. All rights reserved.
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+cd "$(dirname "$0")"
+
+url="${BASE_URL}"
+curl --silent --location --url "$url" 2>/dev/null | xsltproc response.xslt -
+count=$(curl --silent --location --url "$url" 2>/dev/null | xsltproc response.xslt - | grep "<title>Foo")
+[ $count -eq 1 ] || { echo "title not found" && exit 1 ; }
