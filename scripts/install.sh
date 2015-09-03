7a1e5a995a343b82ec0bbec5b3b6f2e79bb34aea
Marcus Rohrmoser <mrohrmoser@acm.org>
2015-09-03 10:52:39 +0200

    travis++

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 cd "$(dirname "$0")/.."
 
-curl --output tarball.tar.gz --url "$SHAARLI_SRC" || { echo "ouch" && exit 1 ; }
+ls -l
+curl --location --output tarball.tar.gz --url "$SHAARLI_SRC" || { echo "ouch" && exit 1 ; }
+ls -l
 
 tar -xzf tarball.tar.gz || { echo "ouch" && exit 2 ; }
+ls -l
 rm tarball.tar.gz
 
 ls -l
