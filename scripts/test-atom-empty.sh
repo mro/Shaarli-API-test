851aba47f998ebe7607edf973932379db73e030c
Marcus Rohrmoser <github@mro.name>
2015-09-04 00:11:15 +0200

    foo

--- scripts/test-atom-empty.sh
+++ scripts/test-atom-empty.sh
@@ -4,8 +4,4 @@ cd "$(dirname "$0")/.."
 entries=-1
 entries=$(curl --silent "$BASE_URL/?do=atom" | xmllint --encode utf8 --format - | grep --count "<entry>")
 
-if [ $entries -eq 1 ] ; then
-  exit 0
-else
-  exit 1
-fi
+[ $entries -eq 1 ] || { echo "expected exactly one <entry>" && exit 1 ; }
