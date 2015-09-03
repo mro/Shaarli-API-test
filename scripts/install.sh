a61847731ac20ffa62947e733b6bcc955b2a52ab
Marcus Rohrmoser <mrohrmoser@acm.org>
2015-09-03 10:48:28 +0200

    travis++

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 cd "$(dirname "$0")/.."
 
-if [ "" = "$SHAARLI_SRC" ] ; then
-	echo "How strange - where is the env" 1>&2
-	SHAARLI_SRC=https://github.com/sebsauvage/Shaarli/archive/master.tar.gz
-fi
-
-curl --output tarball.tar.gz --url "$(SHAARLI_SRC)" || { echo "ouch" && exit 1 ; }
+curl --output tarball.tar.gz --url "$SHAARLI_SRC" || { echo "ouch" && exit 1 ; }
 
 tar -xzf tarball.tar.gz || { echo "ouch" && exit 2 ; }
+rm tarball.tar.gz
 
+ls -l
 ls -l index.php || { echo "ouch" && exit 3 ; }
