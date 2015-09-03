0f73fccd28f87e3fe25cba83a98c6ca4d3c22bf6
Marcus Rohrmoser <github@mro.name>
2015-09-03 23:49:49 +0200

    tests++

--- scripts/test-index.sh
+++ scripts/test-index.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd "$(dirname "$0")/.."
 
-curl --silent "$BASE_URL/" | xmllint --html --encode utf8 --format - >/dev/null
+curl --silent "$BASE_URL/" | xmllint --html --encode utf8 --format - 2>/dev/null >/dev/null
 
 exit $?
