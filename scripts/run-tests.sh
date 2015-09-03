676455ea652a8e2fbad536ebaace31a3ff821c1c
Marcus Rohrmoser <github@mro.name>
2015-09-03 23:10:44 +0200

    initial setup: user+pwd

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -5,7 +5,7 @@ CWD=$(pwd)
 status_code=0
 for tst in ./scripts/test*.sh
 do
-	cd "$CWD"
+  cd "$CWD"
   # prepare a clean test environment from scratch
   rm -rf WebAppRoot
   # ...and unpack into directory 'WebAppRoot'...
@@ -15,8 +15,8 @@ do
 
   ls -l "WebAppRoot/index.php" >/dev/null || { echo "ouch" && exit 2 ; }
 
-  echo "TODO: set up the shaarli instance."
-  echo "TODO: register a user."
+  # curl "$BASE_URL" -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' -H 'Accept-Encoding: gzip, deflate' -H 'Accept-Language: de,en-US;q=0.7,en;q=0.3' -H 'Connection: keep-alive' -H 'Cookie: shaarli=a5929a34b29600fe02d657a976a22664' -H 'Host: shaarli.review.mro.name' -H 'Referer: http://shaarli.review.mro.name/' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:40.0) Gecko/20100101 Firefox/40.0' -H 'Content-Type: application/x-www-form-urlencoded' --data 'setlogin=tast&setpassword=tust&continent=Europe&city=Brussels&title=Review+Shaarli&Save=Save+config'
+  curl "$BASE_URL" -H 'Content-Type: application/x-www-form-urlencoded' --data "setlogin=$USERNAME&setpassword=$PASSWORD&continent=Europe&city=Brussels&title=Review+Shaarli&Save=Save+config"
 
   # execute all tests
   sh "$tst" || { status_code=1 ; }
