6ae12327b185d4b55afaa107db97ad28aaa13bba
Marcus Rohrmoser <mrohrmoser@acm.org>
2015-09-03 11:48:34 +0200

    travis++

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -5,10 +5,14 @@ status_code=0
 for tst in ./scripts/test*.sh
 do
   # prepare a clean test environment from scratch
+  sudo service apache2 stop
+
   rm -rf WebAppRoot
   # ...and unpack into directory 'WebAppRoot'...
   tar -xzf source.tar.gz || { echo "ouch" && exit 1 ; }
   mv $GITHUB_SRC_SUBDIR WebAppRoot
+  sudo service apache2 start
+
   ls -l "WebAppRoot/index.php" >/dev/null || { echo "ouch" && exit 2 ; }
 
   echo "TODO: set up the shaarli instance."
