7dc752598d79fee7b030fd8b774f0ead8841436a
Marcus Rohrmoser <mrohrmoser@acm.org>
2015-09-03 10:39:48 +0200

    add php versions plus ENV test

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 cd "$(dirname "$0")/.."
 
+if [[ ""="$SHAARLI_SRC" ]] ; then
+	echo "How strange - where is the environment." 1>&2
+	SHAARLI_SRC=https://github.com/sebsauvage/Shaarli/archive/master.tar.gz
+fi
+
 curl --output tarball.tar.gz --url "$(SHAARLI_SRC)" || { echo "ouch" && exit 1 ; }
 
 tar -xzf tarball.tar.gz || { echo "ouch" && exit 2 ; }
