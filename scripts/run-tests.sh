b0cbb3a1723dba3f72c10645fb790885d3a2e3c3
Marcus Rohrmoser <github@mro.name>
2015-09-06 00:11:23 +0200

    cosm

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -24,13 +24,34 @@ ruby --version > /dev/null || { echo "I need xmllint." && exit 103 ; }
 cd "$(dirname "$0")/.."
 CWD=$(pwd)
 
+# terminal colors (require bash)
+# http://www.tldp.org/HOWTO/Bash-Prompt-HOWTO/x329.html
+# http://wiki.bash-hackers.org/scripting/terminalcodes
+FGC_NONE="\033[0m"
+FGC_GRAY="\033[1;30m"
+FGC_RED="\033[1;31m"
+FGC_GREEN="\033[1;32m"
+FGC_YELLOW="\033[1;33m"
+FGC_BLUE="\033[1;34m"
+FGC_PURPLE="\033[1;35m"
+FGC_CYAN="\033[1;36m"
+FGC_WHITE="\033[1;37m"
+BGC_GRAY="\033[7;30m"
+BGC_RED="\033[7;31m"
+BGC_GREEN="\033[7;32m"
+BGC_YELLOW="\033[7;33m"
+BGC_BLUE="\033[7;34m"
+BGC_PURPLE="\033[7;35m"
+BGC_CYAN="\033[7;36m"
+BGC_WHITE="\033[7;37m"
+
 status_code=0
 for tst in ./scripts/test*.sh
 do
   test_name="$(basename "$tst")"
   echo -n "travis_fold:start:${test_name}\r"
 
-  printf "Running %s ... " "$test_name"
+  echo "Running $test_name"
   cd "$CWD"
   # prepare a clean test environment from scratch
   rm -rf WebAppRoot
@@ -53,6 +74,7 @@ do
 
   # execute each test
   sh "$tst"
+
   code=$?
   if [ $code -ne 0 ] ; then
     for f in scripts/curl.* WebAppRoot/data/log.txt ; do
@@ -64,9 +86,9 @@ do
   echo -n "travis_fold:end:${test_name}\r"
 
   if [ $code -eq 0 ] ; then
-    echo "success"
+    echo -e "${FGC_GREEN}✓${FGC_NONE} ${test_name}"
   else
-    echo "failed with code: $code"
+    echo -e "${FGC_RED}×${FGC_NONE} ${test_name} with code: $code"
     status_code=1
   fi
 done
