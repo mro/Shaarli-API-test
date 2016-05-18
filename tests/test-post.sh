30b1aec36543e1ec6a223678f892b4c2433b1c7d
Marcus Rohrmoser <github@mro.name>
2016-05-18 22:45:13 +0200

    .⚓️.

--- tests/test-post.sh
+++ tests/test-post.sh
@@ -25,14 +25,14 @@ ruby --version > /dev/null      || assert_fail 103 "I need ruby."
 [ "${USERNAME}" != "" ]         || assert_fail 1 "How strange, USERNAME is unset."
 [ "${PASSWORD}" != "" ]         || assert_fail 2 "How strange, PASSWORD is unset."
 [ "${BASE_URL}" != "" ]         || assert_fail 3 "How strange, BASE_URL is unset."
-[ "" = "$(echo "${BASE_URL}" | egrep -e "/$")" ] || assert_fail 4 "BASE_URL must be without trailing /"
+assert_equal "" "$(echo "${BASE_URL}" | egrep -e "/$")" 28 "BASE_URL must be without trailing /"
 
 echo "###################################################"
 echo "## Non-logged-in Atom feed before adding a link (should have only the initial public default entry):"
 curl --silent --show-error --output curl.tmp.atom "${BASE_URL}/?do=atom"
 xmllint --encode utf8 --format curl.tmp.atom
 entries=$(xmllint --xpath 'count(/*/*[local-name()="entry"])' curl.tmp.atom)
-[ "${entries}" -eq 1 ] || assert_fail 4 "Atom feed expected 1 = ${entries}"
+assert_equal 1 "${entries}" 35 "Atom feed expected 1 = ${entries}"
 
 echo "####################################################"
 echo "## Step 1: fetch token to login and add a new link: "
@@ -49,12 +49,12 @@ LOCATION=$(curl --get --url "${BASE_URL}" \
   --write-out '%{url_effective}' 2>/dev/null)
 # todo:
 errmsg=$(xmllint --html --nowarning --xpath 'string(/html[1 = count(*)]/head[1 = count(*)]/script[starts-with(.,"alert(")])' curl.tmp.html)
-[ "${errmsg}" = "" ] || assert_fail 107 "error: '${errmsg}'"
+assert_equal "" "${errmsg}" 52 "error: '${errmsg}'"
 TOKEN=$(xmllint --html --nowarning --xpath 'string(/html/body//form[@name="loginform"]//input[@name="token"]/@value)' curl.tmp.html)
 # string(..) http://stackoverflow.com/a/18390404
 
 # the precise length doesn't matter, it just has to be significantly larger than ''
-[ $(printf "%s" ${TOKEN} | wc -c) -eq 40 ] || assert_fail 6 "expected TOKEN of 40 characters, but found ${TOKEN} of $(printf "%s" ${TOKEN} | wc -c)"
+assert_equal 40 $(printf "%s" ${TOKEN} | wc -c) 57 "expected TOKEN of 40 characters, but found ${TOKEN} of $(printf "%s" ${TOKEN} | wc -c)"
 
 echo "######################################################"
 echo "## Step 2: follow the redirect, do the login and get the post form: "
@@ -70,15 +70,15 @@ LOCATION=$(curl --url "${LOCATION}" \
   --write-out '%{url_effective}' 2>/dev/null)
 # todo:
 errmsg=$(xmllint --html --nowarning --xpath 'string(/html[1 = count(*)]/head[1 = count(*)]/script[starts-with(.,"alert(")])' curl.tmp.html)
-[ "${errmsg}" = "" ] || assert_fail 108 "error: '${errmsg}'"
+assert_equal "" "${errmsg}" 73 "error: '${errmsg}'"
 # check presence of various mandatory form fields:
 for field in lf_url lf_title lf_linkdate lf_tags token
 do
-  [ $(xmllint --html --nowarning --xpath "count(/html/body//form[@name = 'linkform']//input[@name='${field}'])" curl.tmp.html) -eq 1 ] || assert_fail 8 "expected to have a '${field}'"
+  assert_equal 1 $(xmllint --html --nowarning --xpath "count(/html/body//form[@name = 'linkform']//input[@name='${field}'])" curl.tmp.html) 77 "expected to have a '${field}'"
 done
 for field in lf_description
 do
-  [ $(xmllint --html --nowarning --xpath "count(/html/body//form[@name = 'linkform']//textarea[@name='${field}'])" curl.tmp.html) -eq 1 ] || assert_fail 8 "expected to have a '${field}'"
+  assert_equal 1 $(xmllint --html --nowarning --xpath "count(/html/body//form[@name = 'linkform']//textarea[@name='${field}'])" curl.tmp.html) 81 "expected to have a '${field}'"
 done
 
 # turn form field data into curl post data file
@@ -103,7 +103,7 @@ LOCATION=$(curl --url "${LOCATION}" \
 echo "final ${LOCATION}"
 # todo:
 errmsg=$(xmllint --html --nowarning --xpath 'string(/html[1 = count(*)]/head[1 = count(*)]/script[starts-with(.,"alert(")])' curl.tmp.html 2>/dev/null)
-[ "${errmsg}" = "" ] || assert_fail 107 "error: '${errmsg}'"
+assert_equal "" "${errmsg}" 106 "error: '${errmsg}'"
 echo "${LOCATION}" | egrep -e "^${BASE_URL}/\?#[a-zA-Z0-9@_-]{6}\$" || assert_fail 108 "expected link hash url, but got '${LOCATION}'"
 # don't follow the redirect => no html => no logout link [ 1 -eq "$(xmllint --html --nowarning --xpath "count(/html/body//a[@href = '?do=logout'])" curl.tmp.html 2>/dev/null)" ] || assert_fail 13 "I expected a logout link."
 
@@ -116,4 +116,4 @@ echo "## Non-logged-in Atom feed after adding a link (should have the added + th
 curl --silent --show-error --output curl.tmp.atom "${BASE_URL}/?do=atom"
 xmllint --encode utf8 --format curl.tmp.atom
 entries=$(xmllint --xpath 'count(/*/*[local-name()="entry"])' curl.tmp.atom)
-[ "${entries}" -eq 2 ] || assert_fail 10 "Atom feed expected 2 = ${entries}"
+assert_equal 2 "${entries}" 119 "Atom feed expected 2 = ${entries}"
